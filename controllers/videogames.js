00847e52444f3ed9817d5ee8202ca5ae255f0eaa
Federico Palmieri <federico.palmieri@avalith.net>
2022-01-31 17:51:52 -0300

    adding videogames controller

---
+++ controllers/videogames.js
@@ -0,0 +1,29 @@
+const videogamesService = require('../services/videogames');
+
+const getAll = async (req, res, next) => {
+
+};
+
+const getById = async (req, res, next) => {
+
+};
+
+const create = async (req, res, next) => {
+
+};
+
+const update = async (req, res, next) => {
+
+};
+
+const remove = async (req, res, next) => {
+
+};
+
+module.exports = {
+    getAll,
+    getById,
+    create,
+    update,
+    remove
+};
