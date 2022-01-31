37df0b9bc5b33c0ddb292baee2e94b6c90f22cab
Federico Palmieri <federico.palmieri@avalith.net>
2022-01-31 17:52:52 -0300

    adding character repository

---
+++ repositories/characters.js
@@ -0,0 +1,30 @@
+const Character = require('../models/character');
+
+const getAll = async (limit, offset) => {
+
+};
+
+const getById = async (id) => {
+
+};
+
+const create = async (data) => {
+
+};
+
+const update = async (data, id) => {
+
+};
+
+const remove = async (id) => {
+
+};
+
+module.exports = {
+    getAll,
+    getById,
+    create,
+    update,
+    remove
+};
+
