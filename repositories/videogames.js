32b8b5aa3c57795d65f23c22531cf5c376b41a4c
Federico Palmieri <federico.palmieri@avalith.net>
2022-01-31 17:53:07 -0300

    adding videogame repository

---
+++ repositories/videogames.js
@@ -0,0 +1,29 @@
+const Videogames = require('../models/videogames');
+
+const getAll = async (limit, offset) => {
+
+};
+
+const getById = async (id) => {
+
+};
+
+const create = async (data) => {
+
+};
+
+const update = async (data, id) => {
+
+};
+
+const remove = async (id) => {
+
+};
+
+module.exports = {
+    getAll,
+    getById,
+    create,
+    update,
+    remove
+};
