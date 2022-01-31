23193dc52a5758e3955ad7bafcbf7fb45713dd01
Federico Palmieri <federico.palmieri@avalith.net>
2022-01-31 17:51:33 -0300

    adding character controller

---
+++ controllers/characters.js
@@ -0,0 +1,31 @@
+const charactersService = require('../services/characters');
+
+const getAll = async (req, res, next) => {
+
+};
+
+const getById = async (req, res, next) => {
+
+};
+
+const create = async (req, res, next) => {
+
+};
+
+const update = async (req, res, next) => {
+
+};
+
+const remove = async (req, res, next) => {
+
+};
+
+module.exports = {
+    getAll,
+    getById,
+    create,
+    update,
+    remove
+};
+
+
