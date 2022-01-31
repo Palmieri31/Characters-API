339775258268f3e048ddfcfc074332bb7a8f2356
Federico Palmieri <federico.palmieri@avalith.net>
2022-01-31 17:53:36 -0300

    adding videogame service

---
+++ services/videogames.js
@@ -0,0 +1,29 @@
+const videogamesRepository = require('../repositories/videogames');
+
+const getAll = async () => {
+
+};
+
+const getById = async (id) => {
+
+};
+
+const create = async (videogame) => {
+
+};
+
+const update = async (videogame, id) => {
+
+};
+
+const remove = async (id) => {
+
+};
+
+module.exports = {
+    getAll,
+    getById,
+    create,
+    update,
+    remove
+};
