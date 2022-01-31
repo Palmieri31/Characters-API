e0a19817fca3aa45102c3bd7c788961782434a42
Federico Palmieri <federico.palmieri@avalith.net>
2022-01-31 17:53:22 -0300

    adding character service

---
+++ services/characters.js
@@ -0,0 +1,29 @@
+const charactersRepository = require('../repositories/characters');
+
+const getAll = async () => {
+
+};
+
+const getById = async (id) => {
+
+};
+
+const create = async (character) => {
+
+};
+
+const update = async (character, id) => {
+
+};
+
+const remove = async (id) => {
+
+};
+
+module.exports = {
+    getAll,
+    getById,
+    create,
+    update,
+    remove
+};
