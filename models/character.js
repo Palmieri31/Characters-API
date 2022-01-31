61886f4146ffdefbccb7e4948149b40d4a8e4837
Federico Palmieri <federico.palmieri@avalith.net>
2022-01-31 17:52:09 -0300

    adding character models

---
+++ models/character.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require("mongoose");
+
+const characterSchema = new Schema (
+{
+  name: {
+    type: String,
+  },
+  creator: {
+    type: String,
+  },
+  firstApparition: {
+    type: Date,
+  },
+	imgUrl: {
+		type: String,
+	},
+  Videogames: [
+    {
+      ref: "Videogame",
+			type: Schema.Types.ObjectId    
+    },
+  ],
+},
+{
+  timestamps: true,
+  versionkey: false
+});
+
+module.exports = model('Character', characterSchema);
