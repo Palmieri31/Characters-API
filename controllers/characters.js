33f49b26e8b7649d77c927082dfb41ad1ba60338
Federico Palmieri <federico.palmieri@avalith.net>
2022-02-01 17:58:18 -0300

    crud character in controllers

--- controllers/characters.js
+++ controllers/characters.js
@@ -5,19 +5,53 @@ const getAll = async (req, res, next) => {
 };
 
 const getById = async (req, res, next) => {
-
+  try {
+    const { characterId } = req.params;
+    const character = await charactersService.getById(characterId);
+    res.status(200).json(character);
+  } catch (error) {
+    next(error);  
+  }
 };
 
 const create = async (req, res, next) => {
-
+  try {
+    const response = await charactersService.create(req.body);
+    res.status(200).json({
+      success: true,
+      msg: ` character: ${response.name} has been created`,
+      character: response
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 const update = async (req, res, next) => {
-
+  try {
+    const { characterId } = req.params;
+    const response = await charactersService.update(req.body, characterId);
+    res.status(200).json({
+        success: true,
+        msg: ` character: ${response.name} has been updated`,
+        character: response
+      });
+  } catch (error) {
+    next(error);
+  }
 };
 
 const remove = async (req, res, next) => {
-
+  try {
+    const { characterId } = req.params;
+    await charactersService.remove(characterId);
+    res.status(200).json({
+        success: true,
+        msg: ` character has been deleted`,
+    });
+  }catch (error) {
+    next(error);
+  }
 };
 
 module.exports = {
