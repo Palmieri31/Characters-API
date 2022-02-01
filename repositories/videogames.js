87480e995c6c2265f7faab154efb0400b0107558
Federico Palmieri <federico.palmieri@avalith.net>
2022-02-01 18:00:49 -0300

    crud in videogames repositories

--- repositories/videogames.js
+++ repositories/videogames.js
@@ -1,23 +1,29 @@
-const Videogames = require('../models/videogames');
+const Videogame = require('../models/videogame');
 
 const getAll = async (limit, offset) => {
 
 };
 
 const getById = async (id) => {
-
+  const videogame = await Videogame.findById(id);
+  return videogame;
 };
 
-const create = async (data) => {
-
+const create = async (newVideogame) => {
+  const videogame = await newVideogame.save();
+  return videogame;
 };
 
-const update = async (data, id) => {
-
+const update = async (id, data) => {
+  const updateVideogame = await Videogame.findByIdAndUpdate(id, data, {
+    new: true
+  });
+  return updateVideogame;
 };
 
 const remove = async (id) => {
-
+  const response = await Videogame.findByIdAndDelete(id);
+  return response; 
 };
 
 module.exports = {
