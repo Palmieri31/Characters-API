d9018aa744d8f3afdf1c5a6d37c54524db9bea3b
Federico Palmieri <federico.palmieri@avalith.net>
2022-02-01 18:00:31 -0300

    crud in character repositories

--- repositories/characters.js
+++ repositories/characters.js
@@ -5,19 +5,25 @@ const getAll = async (limit, offset) => {
 };
 
 const getById = async (id) => {
-
+    const character = await Character.findById(id);
+    return character;
 };
 
-const create = async (data) => {
-
+const create = async (newCharacter) => {
+    const character = await newCharacter.save();
+    return character;
 };
 
-const update = async (data, id) => {
-
+const update = async (id, data) => {
+    const updateCharacter = await Character.findByIdAndUpdate(id, data, {
+        new: true
+      });
+      return updateCharacter;
 };
 
 const remove = async (id) => {
-
+    const response = await Character.findByIdAndDelete(id);
+    return response; 
 };
 
 module.exports = {
