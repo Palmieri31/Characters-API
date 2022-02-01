94ccdf382a86af02fb2818201f4b27f72bfbca72
Federico Palmieri <federico.palmieri@avalith.net>
2022-02-01 17:58:39 -0300

    crud videogames in controllers

--- controllers/videogames.js
+++ controllers/videogames.js
@@ -5,19 +5,53 @@ const getAll = async (req, res, next) => {
 };
 
 const getById = async (req, res, next) => {
-
+  try {
+    const { videogameId } = req.params;
+    const videogame = await videogamesService.getById(videogameId);
+    res.status(200).json(videogame);
+  } catch (error) {
+    next(error);  
+  }
 };
 
 const create = async (req, res, next) => {
-
+  try {
+    const response = await videogamesService.create(req.body);
+    res.status(200).json({
+      success: true,
+      msg: `${response.title} has been created`,
+      videogame: response
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 const update = async (req, res, next) => {
-
+  try {
+    const { videogameId } = req.params;
+    const response = await videogamesService.update(req.body, videogameId);
+    res.status(200).json({
+      success: true,
+      msg: `${response.title} has been updated`,
+      videogame: response
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 const remove = async (req, res, next) => {
-
+  try {
+    const { videogameId } = req.params;
+    await videogamesService.remove(videogameId);
+    res.status(200).json({
+      success: true,
+      msg: `videogame has been deleted`,
+    });
+  }catch (error) {
+    next(error);
+  }
 };
 
 module.exports = {
