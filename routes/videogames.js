04358575f954499d0da77bd3670aa2616d0735cd
Federico Palmieri <federico.palmieri@avalith.net>
2022-02-02 18:04:57 -0300

    adding routes to videogames

--- routes/videogames.js
+++ routes/videogames.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const videogamesController = require('../controllers/videogames');
+const authsMiddleware = require('../middlewares/auths');
+const videogamesMiddleware = require('../middlewares/videogames');
 const videogamesRouter = express.Router();
 
-videogamesRouter.post('/', videogamesController.create);
-videogamesRouter.get('/:videogameId', videogamesController.getById);
-videogamesRouter.patch('/:videogameId', videogamesController.update);
-videogamesRouter.delete('/:videogameId', videogamesController.remove);
+videogamesRouter.post('/', videogamesMiddleware.validateVideogame, authsMiddleware.isAdmin, videogamesController.create);
+videogamesRouter.get('/:videogameId',authsMiddleware.isLoggedUser, videogamesController.getById);
+videogamesRouter.patch('/:videogameId', authsMiddleware.isAdmin, videogamesController.update);
+videogamesRouter.delete('/:videogameId', authsMiddleware.isAdmin, videogamesController.remove);
 
 module.exports = videogamesRouter;
