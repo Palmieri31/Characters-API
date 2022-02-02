d3c602fd1457f891d95351ba7c163c4ff81a561c
Federico Palmieri <federico.palmieri@avalith.net>
2022-02-02 18:03:01 -0300

    adding character middleware

---
+++ middlewares/characters.js
@@ -0,0 +1,37 @@
+const { check, validationResult } = require('express-validator');
+
+const errorHandler = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+		return res.status(422).json({ errors: errors.array() });
+  }
+	next();
+};
+
+const validateCharacter = [
+	check('name')
+		.notEmpty()
+		.withMessage('You need to enter a name')
+		.bail(),
+
+	check('creator')
+		.notEmpty()
+		.withMessage('You need to enter a creator')
+		.bail(),
+
+	check('firstApparition')
+		.notEmpty()
+		.withMessage('You need to enter a date!')
+        .isDate()
+        .withMessage('first apparition is not a date'),
+		
+    check('imgUrl')
+		.notEmpty()
+		.withMessage('You need to enter a imgUrl'),
+
+	errorHandler
+];
+
+module.exports = {
+	validateCharacter
+};
